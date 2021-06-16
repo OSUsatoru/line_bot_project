ed7b6c539f2c65349e0d7249440caa5b8e994211
OSUsatoru <yamamsat@oregonstate.edu>
2021-06-16 10:02:52 -0700

    test

--- main.py
+++ main.py
@@ -42,10 +42,34 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    buttons_template_message = TemplateSendMessage(
+        alt_text='Buttons template',
+        template=ButtonsTemplate(
+            thumbnail_image_url='https://example.com/image.jpg',
+            title='Menu',
+            text='Please select',
+            actions=[
+                PostbackAction(
+                    label='postback',
+                    display_text='postback text',
+                    data='action=buy&itemid=1'
+                ),
+                MessageAction(
+                    label='message',
+                    text='message text'
+                ),
+                URIAction(
+                    label='uri',
+                    uri='http://example.com/'
+                )
+            ]
+        )
+    )
+
     line_bot_api.reply_message(
         event.reply_token,
         #TextSendMessage(text=event.message.text))
-        TextSendMessage(text="test"))
+        TextSendMessage(text=buttons_template_message))
 
 
 if __name__ == "__main__":
